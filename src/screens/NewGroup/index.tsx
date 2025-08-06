82bd677ecdd803c104cb6d78b78b93cedf3c7a3b
Bulani <jao_1999@hotmail.com>
2025-08-06 18:06:17 -0300

    new: adicionando navegacao

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -1,10 +1,23 @@
-import { Highlight } from "@components/Highlight";
+import { useState } from "react";
+
+import { useNavigation } from "@react-navigation/native";
+
 import { Container, Content, Icon } from "./styles";
+
+import { Highlight } from "@components/Highlight";
 import { Header } from "@components/Header";
 import { Button } from "@components/Button";
 import { Input } from "@components/Input";
 
 export function NewGroup() {
+  const [group, setGroup] = useState('');
+
+  const navigation = useNavigation();
+  
+  function handleNew(){
+    navigation.navigate('players', { group })
+  }
+  
   return (
     <Container>
       <Header showBackButton />
@@ -19,11 +32,13 @@ export function NewGroup() {
 
         <Input
           placeholder="Nome da turma"
+          onChangeText={setGroup}
         />
 
         <Button 
           title="Criar"
           style={{ marginTop: 20 }}
+          onPress={handleNew}
         />
 
       </Content>
--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { useRoute } from "@react-navigation/native";
 
 import { FlatList } from "react-native";
 
@@ -11,18 +12,25 @@ import { PlayerCard } from "@components/PlayerCard";
 import { ListEmpty } from "@components/ListEmpty";
 import { Button } from "@components/Button";
 
-import { Container, Form, HeaderList, NumbersOfPlayers } from "./styles";
+import { Container, Form, HeaderList, NumbersOfPlayer } from "./styles";
+
+type RouteParams = {
+  group: string;
+}
 
 export function Players(){
   const [team, setTeam] = useState('Time A')
   const [players, setPlayers] = useState([])
+
+  const route = useRoute();
+  const { group } = route.params as RouteParams;
 
   return(
     <Container>
       <Header showBackButton />
 
       <Highlight 
-        title="Nome da turma"
+        title={group}
         subtitle="adicione a galera e separe os times"
       />
 
@@ -51,9 +59,9 @@ export function Players(){
           horizontal
         />
 
-        <NumbersOfPlayers>
+        <NumbersOfPlayer>
           {players.length}
-        </NumbersOfPlayers>
+        </NumbersOfPlayer>
       </HeaderList>
 
       <FlatList 
